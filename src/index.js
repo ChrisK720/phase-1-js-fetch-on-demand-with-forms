41d1bbf1ce1cb9f304ef9c1d35cc7af137ad9733
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-01-11 21:50:31 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
+   const idForm = document.querySelector('form')
+   idForm.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    const userInp = document.getElementById('searchByID')
+    fetch(`http://localhost:3000/movies`)
+    .then(res => res.json())
+    .then(data => {
+        let title = document.querySelector('section#movieDetails h4')
+        let summary = document.querySelector('section#movieDetails p')
+        title.textContent = data[userInp.value].title
+        summary.textContent = data[userInp.value].summary
+    })
+   })
   
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init());
